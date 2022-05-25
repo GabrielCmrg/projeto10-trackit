63639ba5e00be4e28778739afdd4e1df5fc61d8d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-05-25 16:39:12 -0300

    footer added to today route

--- src/components/TodayScreen.js
+++ src/components/TodayScreen.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+import 'react-circular-progressbar/dist/styles.css';
 
 import ApplicationContext from "../contexts/ApplicationContext";
 
@@ -7,14 +11,30 @@ export default function TodayScreen() {
     const { loginInfo } = React.useContext(ApplicationContext);
 
     return(
-        <>
+        <Body>
             <Header>
                 <div className="site-title">TrackIt</div>
                 <ProfilePhoto src={loginInfo.image} alt="profile" />
             </Header>
-        </>
+            <Footer>
+                <Link to="/habitos">
+                    Hábitos
+                </Link>
+                <ProgressBar>
+                    <CircularProgressbar value={20} text={"Hoje"} background backgroundPadding={6}/>
+                </ProgressBar>
+                <Link to="/historico">
+                    Histórico
+                </Link>
+            </Footer>
+        </Body>
     );
 }
+
+const Body = styled.div`
+    background-color: #F2F2F2;
+    height: 100vh;
+`;
 
 const Header = styled.div`
     position: fixed;
@@ -30,6 +50,7 @@ const Header = styled.div`
     color: white;
     padding: 0 18px;
     box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 `;
 
 const ProfilePhoto = styled.img`
@@ -37,4 +58,43 @@ const ProfilePhoto = styled.img`
     width: 51px;
     height: 51px;
     object-fit: cover;
+`;
+
+const Footer = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 70px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    box-sizing: border-box;
+`;
+
+const ProgressBar = styled.div`
+    width: 90px;
+    height: 90px;
+    position: absolute;
+    bottom: 10px;
+    left: calc(50% - 45px);
+
+    .CircularProgressbar-path {
+        stroke: white;
+    }
+
+    .CircularProgressbar-trail {
+        stroke: #52B6FF;
+    }
+
+    .CircularProgressbar-text {
+        fill: white;
+        font-size: 18px;
+    }
+
+    .CircularProgressbar-background {
+        fill: #52B6FF;
+    }
 `;
